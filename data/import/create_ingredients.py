bba3710e1a91062a05483f74c601e7fa9b9d5950
Laurent Aubin <laurent.aubin.2@ulaval.ca>
2021-10-11 16:54:34 -0400

    Import data in neo4j

---
+++ data/import/create_ingredients.py
@@ -0,0 +1,21 @@
+import json
+
+
+def main():
+    ingredients = set()
+    with open("cocktails.json", "r") as f:
+        cocktails = json.load(f)
+
+        for cocktail in cocktails:
+            for i in range(1, 16):
+                ingredient = cocktail.get(f"strIngredient{i}")
+                ingredients.add(ingredient)
+
+    ingredients.remove("")
+    ingredients.remove(None)
+    with open("ingredients.json", "w") as f:
+        json.dump(list(ingredients), f)
+
+
+if __name__ == "__main__":
+    main()
---
+++ data/populate_neo4j.py
@@ -0,0 +1,73 @@
+import json
+from py2neo import Graph, Node, Relationship
+
+URL = "bolt://localhost:7687"
+USERNAME = "neo4j"
+PASSWORD = "password"
+
+
+class GraphPopulator:
+    def __init__(self, db_url: str, username: str, password: str):
+        self.graph = Graph(db_url, auth=(username, password), secure=False)
+
+    def populate_ingredients(self, ingredients: list):
+        transaction = self.graph.begin()
+
+        for ingredient in ingredients:
+            node = Node("Ingredient", name=ingredient)
+            transaction.create(node)
+
+        self.graph.commit(transaction)
+
+    def populate_cocktails(self, cocktails: list):
+        transaction = self.graph.begin()
+
+        for cocktail in cocktails:
+            node = Node("Cocktail", id=cocktail.get("id"), name=cocktail.get("name"), instructions=cocktail.get("instructions"),
+                        glass=cocktail.get("glass"), thumbnail=cocktail.get("thumbnail"))
+            transaction.create(node)
+
+        self.graph.commit(transaction)
+
+    def create_relations(self, cocktails: list):
+        transaction = self.graph.begin()
+
+        for cocktail in cocktails:
+            for ingredient in cocktail.get("ingredients"):
+                cocktail_request = self.graph.run(f"MATCH (cocktail:Cocktail {{ id: '{cocktail.get('id')}' }}) RETURN cocktail")
+                cocktail_node = [x for x in cocktail_request][0]["cocktail"]
+
+                ingredient_request = self.graph.run(
+                    f"MATCH (ingredient:Ingredient {{ name: '{ingredient.get('name')}' }}) RETURN ingredient")
+                ingredient_node = [x for x in ingredient_request][0]["ingredient"]
+
+                rel = Relationship(cocktail_node, "made_with", ingredient_node, quantity=ingredient.get("quantity"))
+                transaction.create(rel)
+
+        self.graph.commit(transaction)
+
+
+def import_ingredients():
+    with open("import/ingredients.json") as f:
+        return json.load(f)
+
+
+def import_cocktails():
+    with open("import/cocktails.json") as f:
+        return json.load(f)
+
+
+def main():
+    populator = GraphPopulator(URL, USERNAME, PASSWORD)
+
+    ingredients = import_ingredients()
+    # populator.populate_ingredients(ingredients)
+
+    cocktails = import_cocktails()
+    # populator.populate_cocktails(cocktails)
+
+    populator.create_relations(cocktails)
+
+
+if __name__ == "__main__":
+    main()
